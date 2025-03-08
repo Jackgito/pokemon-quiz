d3846807f34e0159443271fea247b32b8bb66f8d
Juhani Manninen <juhani.manninen00@gmail.com>
2025-03-08 19:42:54 +0200

    Fix Vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,11 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [
+      'https://whos-that-pokemon-psi.vercel.app/',
+      'http://localhost:5173'
+  ],
+    // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
 }));
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   cors: true,
+  server: {
+    cors: true,
     // proxy: {
     //   '/api': {
     //     target: 'http://localhost:8080',
@@ -15,5 +15,5 @@ export default defineConfig({
     //     proxyTimeout: 120000
     //   }
     // }
-  // }
+  }
 })

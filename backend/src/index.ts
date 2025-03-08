7435c96dd5fd49b95271d8c2967417a055f4dad8
Juhani Manninen <juhani.manninen00@gmail.com>
2025-03-08 20:04:48 +0200

    Fix Vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,11 @@ app.use(cors({
   ],
     // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.options('*', cors());
 
 app.use(compression());
 app.use(cookieParser());
